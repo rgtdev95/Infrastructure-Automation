5ec357dc5b47726ab43fbf95515c3d9910dab8eb
Ron T. <ron@rtg-homelabs.tech>
2025-11-29 09:34:29 +0800

    refactor: simplify `test_connection_script.sh` by removing example output and adding a shebang.

--- Ansible/test_connection_script.sh
+++ Ansible/test_connection_script.sh
@@ -1,17 +1,2 @@
+#!/bin/bash
 ansible all -m ping
-```
-
-You should see green "SUCCESS" for all three machines:
-```
-control-machine | SUCCESS => {
-    "changed": false,
-    "ping": "pong"
-}
-server1 | SUCCESS => {
-    "changed": false,
-    "ping": "pong"
-}
-server2 | SUCCESS => {
-    "changed": false,
-    "ping": "pong"
-}
